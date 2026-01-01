7c78fe67cab32ce8b5f572b9da5978c0da0c5089
BlueSchmitz <lismitz99@googlemail.com>
2026-01-01 13:14:38 +0100

    sanity_checks with amber tip3p

--- MD_simulations/projects/5EKY_amber_TIP3P/bash_scripts/S_sanity_checks_apptainer_ubuntusmpi.sh
+++ MD_simulations/projects/5EKY_amber_TIP3P/bash_scripts/S_sanity_checks_apptainer_ubuntusmpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 
 #SBATCH -J sanity_checks_Ec5EKYm_apptainer  
-#SBATCH -t 00:05:00
+#SBATCH -t 01:00:00
 #SBATCH -p rome
 #SBATCH -N 1
 #SBATCH -n 16
@@ -35,18 +35,48 @@ module load matplotlib/3.7.2-gfbf-2023a
 #echo "Contents of $TMPDIR:"
 #tree -a -L 10 $TMPDIR
 #cd $TMPDIR/MD_simulations/projects/5EKY_monomeric/outputs
-cd $HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/5EKY_monomeric/outputs
 
 # Set paths for mdp_templates, force_fields and pdb file (to change quickly)
-export GMXLIB=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/force_fields # make sure this is correct
-project=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/5EKY_monomeric/outputs
 mdp=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/mdp_templates
-pdb=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/5EKY_monomeric/inputs # Input PDB file (with correct protonation states)
-pyscripts=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/scripts
+scripts=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/scripts
+
+### Prepare HREX ###
+echo "Sanity checks"
+cp ./outputs/4_equilibration/npt_5.gro ./outputs/6_HREX/npt_5.gro
+cp ./outputs/4_equilibration/topol_5.top ./outputs/6_HREX/topol_5.top
+cd ./6_HREX
+# Files we need: npt_5.gro as the starting structure, topol.top without pointers to posre.itp (no more restraints)
+# 1. Remove constraints from topol.top by commenting out the line that includes posre_5.itp
+cp topol_5.top topol_prod.top
+sed -i '/#ifdef POSRES/,/#endif/d' topol_prod.top # Remove the protein restraint block (POSRES)
+echo "Removed position restraints from topol_prod.top."
+# 2. Re-name topology file (already self-cotained)
+mv topol_prod.top processed.top
+echo "processed.top is self-contained."
+# 3. Edit the processed.top file to indicate which atoms we want to scale (marked with an _ after the residue name)
+python $scripts/scale_residues.py > processed_scaled.top
+echo "Generated processed_scaled.top with selected residues for scaling."
+# 4. Scale the Hamiltonian of the selected atoms by the factors 1.00, 0.95, 0.91, 0.87, 0.83, 0.79, 0.76, 0.72, 0.69, 0.66, 0.63, and 0.60
+for i in 1.00 0.95 0.91 0.87 0.83 0.79 0.76 0.72 0.69 0.66 0.63 0.60;
+do 
+  mkdir -p ./rep${i} # create directory for each replica
+  plumed partial_tempering ${i} < processed_scaled.top  > ./rep${i}/scaled_${i}.top
+  echo "Generated scaled_${i}.top in ./rep${i} with scaling factor ${i}."
+  cd ./rep${i}
+  gmx_mpi grompp -f ../$mdp/hrex.mdp -c ../npt_5.gro -p scaled_${i}.top -o topol.tpr -maxwarn 1
+  echo "Generated topol.tpr from scaled_${i}.top in ./rep${i}."
+  cd ..
+done 
+echo "Generated all scaled topology files and tpr files for HREX replicas."
+# 5. Empty plumed.dat file (no additional CVs, only HREX)
+: > plumed.dat # empty plumed file
 
 ### Sanity checks ###
-echo "Sanity checks"
-cd 5_sanity_checks
+cp ./npt_5.gro ../5_sanity_checks/npt_5.gro
+cp ./processed.top ../5_sanity_checks/processed.top
+cp ./rep1.00/scaled_1.00.top ../5_sanity_checks/scaled_1.00.top
+cd ../5_sanity_checks
+
 # 1. Sanity check of created topologies: compare energies between original and 1.00 scaled system
 # produce tpr + short trajectory (use processed.top)
 apptainer exec $GROMACS_CONTAINER gmx_mpi grompp -f $mdp/sanity_check.mdp -c npt_5.gro -p processed.top -o make_traj.tpr -maxwarn 1
@@ -63,12 +93,17 @@ apptainer exec $GROMACS_CONTAINER mpirun -np $OMP_NUM_TASKS gmx_mpi mdrun -rerun
 # compare energies
 echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10" | apptainer exec $GROMACS_CONTAINER gmx_mpi energy -f ener_pro.edr -o energies_pro.xvg -xvg none
 echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10" | apptainer exec $GROMACS_CONTAINER gmx_mpi energy -f ener_scaled_1.00.edr -o energies_scaled_1.00.xvg -xvg none
-python $pyscripts/energy_comparison.py energies_pro.xvg energies_scaled_1.00.xvg > energy_diff_1.00.log
+python $scripts/energy_comparison.py energies_pro.xvg energies_scaled_1.00.xvg > energy_diff_1.00.log
+
 # 2. Sanity check of created topologies: compare energies between 1.00 scaled system and 0.5 scaled system
+# scale all residues to 0.5
+python $scripts/scale_residues.py > processed_scaled_all.top
+plumed partial_tempering 0.5 < processed_scaled_all.top  > scaled_0.5_all.top
 apptainer exec $GROMACS_CONTAINER gmx_mpi grompp -f $mdp/sanity_check.mdp -c ./npt_5.gro -p ./scaled_0.5_all.top -o scaled_0.5_all.tpr -maxwarn 2
 apptainer exec $GROMACS_CONTAINER mpirun -np $OMP_NUM_TASKS gmx_mpi mdrun -rerun traj.xtc -s scaled_0.5_all.tpr -e ener_scaled_0.5_all.edr -g rerun_scaled_0.5_all.log -ntomp $OMP_NUM_THREADS
 echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10" | apptainer exec $GROMACS_CONTAINER gmx_mpi energy -f ener_scaled_0.5_all.edr -o energies_scaled_0.5_all.xvg -xvg none
-python $pyscripts/energy_comparison.py energies_scaled_1.00.xvg energies_scaled_0.5_all.xvg > energy_diff_0.5_all.log
+python $scripts/energy_comparison.py energies_scaled_1.00.xvg energies_scaled_0.5_all.xvg > energy_diff_0.5_all.log
+
 # 3. Sanity check of replica-exchange implementation
 # Run a short HREX with two equivalent topology files (topol.tpr and scaled_1.00.tpr)
 mkdir -p ./rep0 ./rep1
--- MD_simulations/scripts/scale_residues.py
+++ MD_simulations/scripts/scale_residues.py
@@ -2,7 +2,7 @@
 # usage: python scale_residues.py > processed_scaled.top
 '''Adds "_" to specific atom names in processed.top'''
 
-# list with all residues named in the paper to be scaled
+# select residues to be scaled
 residues_to_scale = list(range(19,26)) + list(range(75,83)) + \
                     list(range(168,179)) + list(range(202,209)) + \
                     list(range(249,260))
