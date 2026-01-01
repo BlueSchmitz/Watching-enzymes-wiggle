4c849e92656dee749882c5feb348b9066c8bcc74
BlueSchmitz <lismitz99@googlemail.com>
2026-01-01 21:20:29 +0100

    add micopy module

--- MD_simulations/projects/5EKY_monomeric/bash_scripts/S_Optimization.sh
+++ MD_simulations/projects/5EKY_monomeric/bash_scripts/S_Optimization.sh
@@ -36,6 +36,7 @@ export GROMACS_CONTAINER=$HOME/Blue/software/apptainer_2021/gromacs_plumed.sif
 # Load modules:  
 module load 2023
 #module load OpenMPI/4.1.5-GCC-12.3.0
+module load mpicopy/4.2-gompi-2024a
 module load matplotlib/3.7.2-gfbf-2023a
 
 # Copy input files to scratch
