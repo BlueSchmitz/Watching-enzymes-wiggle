381b66bbf2e1cad048e1c914fb547d1e99fecfb3
BlueSchmitz <lismitz99@googlemail.com>
2026-01-21 13:28:49 +0100

    fix file naming

--- MD_simulations/projects/1JCLd/bash_scripts/plumed_sanity_checks.sh
+++ MD_simulations/projects/1JCLd/bash_scripts/plumed_sanity_checks.sh
@@ -200,7 +200,7 @@ apptainer exec $PLUMED_CONTAINER plumed partial_tempering 0.5 < processed_scaled
 apptainer exec $GROMACS_CONTAINER gmx_mpi grompp -f $mdp/sanity_check.mdp -c ./npt_5.gro -p ./scaled_0.5_all.top -o scaled_0.5_all.tpr -maxwarn 2
 apptainer exec $GROMACS_CONTAINER gmx_mpi mdrun -rerun traj.xtc -s scaled_0.5_all.tpr -e ener_scaled_0.5_all.edr -g rerun_scaled_0.5_all.log
 echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n10" | apptainer exec $GROMACS_CONTAINER gmx_mpi energy -f ener_scaled_0.5_all.edr -o energies_scaled_0.5_all.xvg -xvg none
-python $scripts/energy_comparison.py energies_scaled_1.00_.xvg energies_scaled_0.5_all.xvg > energy_diff_0.5_all.log
+python $scripts/energy_comparison.py energies_scaled_1.00.xvg energies_scaled_0.5_all.xvg > energy_diff_0.5_all.log
 # 3. Sanity check of replica-exchange implementation
 # Run a short HREX with two equivalent topology files (topol.tpr and scaled_1.00.tpr)
 mkdir -p ./rep0 ./rep1
