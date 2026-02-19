2cc54b43b442b3b84b607580925c6c4cb904b140
BlueSchmitz <lismitz99@googlemail.com>
2026-02-19 16:50:21 +0100

    different downsampling

--- MD_simulations/projects/1JCLm/bash_scripts/save_trajectory_SN.sh
+++ MD_simulations/projects/1JCLm/bash_scripts/save_trajectory_SN.sh
@@ -65,5 +65,5 @@ trap copy_back_results EXIT
 echo "============= Downsizing and Exporting trajectory ============="
 echo -e "q" | apptainer exec $GROMACS_CONTAINER gmx_mpi make_ndx -f topol.tpr -o index.ndx # make index file with default groups
 # downsample trajectory 
-echo -e "0\n0" | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f traj_comp.xtc -s topol.tpr -n index.ndx -o md_200.xtc -dt 200
-echo "Trajectory saved as md_200.xtc"
+echo -e "0\n0" | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f traj_comp.xtc -s topol.tpr -n index.ndx -o md_1000.xtc -dt 1000
+echo "Trajectory saved as md_1000.xtc"
