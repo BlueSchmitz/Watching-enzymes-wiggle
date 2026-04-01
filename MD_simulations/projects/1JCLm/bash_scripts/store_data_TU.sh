d6068fd391a4666657abd7c49f1dac7adffb7e42
BlueSchmitz <lismitz99@googlemail.com>
2026-04-01 15:46:25 +0200

    copy over more stuff

--- MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
+++ MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
@@ -11,8 +11,8 @@
 #SBATCH --mail-user=blueschmitz@tudelft.nl
 #SBATCH --output=copy_to_TU_%j.out
 
-SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/1JCLd/outputs/"
-DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/EcDERA/Dimer"
+SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/5EKY_monomeric/outputs/"
+DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/EcDERA/Old_5EKY"
 
 echo "HOSTNAME: $(hostname)"
 echo "PATH: $PATH"
