0733ed6a8c498965122cca8d2a20e6c8d2c8ef1e
BlueSchmitz <lismitz99@googlemail.com>
2026-04-08 11:05:34 +0200

    MlDERA hrex

---
+++ MD_simulations/projects/MlDERA/bash_scripts/hrex_production.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#SBATCH -J MlDERA_HREX
+#SBATCH -t 48:00:00
+#SBATCH -p rome
+#SBATCH -N 1
+#SBATCH -n 120
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=0
+#SBATCH --output=MlDERA_HREX_%j.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=blueschmitz@tudelft.nl
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_TASKS=$SLURM_NTASKS
+
+# Exit immediately on errors, undefined vars, or failed pipes
+set -euo pipefail
+set -o errtrace
+
+### Project-specific settings ###
+project_dir=MlDERA
+output_dir=6_HREX
+pH=7
+
+export GMXLIB=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/force_fields
+export GROMACS_CONTAINER=$HOME/Blue/software/apptainer_2021/gromacs_plumed.sif
+export PDB2PQR_CONTAINER=$HOME/Blue/software/apptainer_pdb2pqr/pdb2pqr.sif
+export PLUMED_CONTAINER=$HOME/Blue/software/apptainer_plumed/plumed.sif
+
+mdp=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/mdp_templates
+scripts=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/scripts
+pdb=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/$project_dir/inputs/*.pdb
+
+# Create temporary directory on scratch for this job
+tmpdir=$(mktemp -d /gpfs/scratch1/shared/rleveson/Blue/tmp.XXXXXX)
+mkdir -p "$tmpdir/MD_simulations/projects/"
+
+# Load modules:  
+module load 2023
+module load matplotlib/3.7.2-gfbf-2023a
+module list
+
+### Copy project to scratch ###
+echo "=== Copying project to scratch ==="
+cp -r $HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/$project_dir "$tmpdir/MD_simulations/projects/"
+cd $tmpdir/MD_simulations/projects/$project_dir
+
+# Function to copy back results when error occurs and before the script exits
+function copy_back_results {
+    set +e +u # Disable exit on error for this function
+    echo "=== Copying results back to home at $(date). ==="
+    if [[ -d "$tmpdir/MD_simulations/projects/$project_dir/outputs/$output_dir" ]]; then
+        rsync -av --partial --inplace \
+          "$tmpdir/MD_simulations/projects/$project_dir/outputs/$output_dir/" \
+          "$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/$project_dir/outputs/$output_dir/"
+        echo "=== Copy complete at $(date) ==="
+    else
+        echo "Nothing to copy back (outputs directory not found)"
+    fi
+    # Clean up temporary directory
+    rm -rf "$tmpdir"
+    echo "=== Temporary directory cleaned up at $(date). ==="
+}
+trap copy_back_results EXIT
+
+mkdir -p ./outputs/$output_dir
+cd ./outputs/$output_dir/
+
+### Run HREX ###
+apptainer exec $GROMACS_CONTAINER mpirun -np 120 \
+    gmx_mpi mdrun \
+    -multidir rep* \
+    -replex 2000 \
+    -plumed ../plumed.dat \
+    -cpt 15 \
+    -ntomp 1 \
+    -hrex \
+    -maxh 47 \
+    -cpi state.cpt
+
+echo "mdrun exited at $(date)"
