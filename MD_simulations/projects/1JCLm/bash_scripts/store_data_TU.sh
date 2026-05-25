5fbb797389aebb71826065223038745d4fef21a6
BlueSchmitz <lismitz99@googlemail.com>
2026-05-25 12:37:01 +0200

    copy traj BtDERA

--- MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
+++ MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
@@ -11,10 +11,10 @@
 #SBATCH --mail-user=blueschmitz@tudelft.nl
 #SBATCH --output=copy_to_TU_%j.out
 
-#SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/CbDERA"
-SRC="/gpfs/work1/0/prjs2080/CbDERA/outputs/6_HREX/rep1.00"
-#DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/CbDERA"
-DEST="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/CbDERA/outputs/6_HREX/rep1.00"
+#SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA"
+SRC="/gpfs/work1/0/prjs2080/BtDERA/outputs/6_HREX/rep1.00"
+#DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA"
+DEST="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA/outputs/6_HREX/rep1.00"
 
 echo "HOSTNAME: $(hostname)"
 echo "PATH: $PATH"
@@ -22,6 +22,33 @@ which rclone || echo "RCLONE NOT FOUND"
 rclone version || echo "RCLONE NOT RUNNING"
 
 rclone copy "$SRC" "$DEST" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
+
+SRC2="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA"
+DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA"
+
+rclone copy "$SRC2" "$DEST2" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
+
+
+SRC3="/gpfs/work1/0/prjs2080/BtDERA/outputs/6_HREX"
+DEST3="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA/outputs/6_HREX"
+
+rclone copy "$SRC3" "$DEST3" \
     -P \
     -vv \
     --transfers=8 \
