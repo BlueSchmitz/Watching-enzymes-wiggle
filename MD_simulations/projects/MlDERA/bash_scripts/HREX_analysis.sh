ff36bc3b5aa8d392f411cb5dc36011760301a718
BlueSchmitz <lismitz99@googlemail.com>
2026-05-25 09:42:18 +0200

    fixed error

--- MD_simulations/projects/MlDERA/bash_scripts/HREX_analysis.sh
+++ MD_simulations/projects/MlDERA/bash_scripts/HREX_analysis.sh
@@ -146,21 +146,22 @@ EOF
 #echo 23 23 | apptainer exec $GROMACS_CONTAINER gmx_mpi rms -s $tpr -f md_fit.xtc -n index.ndx -o rmsd_tail_backbone.xvg -tu ns
 #python $scripts/plotxvg.py rmsd_tail_backbone.xvg 
 # calculate distance between Lys166 NZ and Asp229 OH over time, output in xvg format
-apptainer exec $GROMACS_CONTAINER gmx_mpi distance -s $tpr -f md_fit.xtc -n index.ndx -oall lys166_asp229_distance.xvg -tu ns -select 'com of group 24 plus com of group 25'
-python $scripts/plotxvg.py lys166_asp229_distance.xvg
+#apptainer exec $GROMACS_CONTAINER gmx_mpi distance -s $tpr -f md_fit.xtc -n index.ndx -oall lys166_asp229_distance.xvg -tu ns -select 'com of group 24 plus com of group 25'
+#python $scripts/plotxvg.py lys166_asp229_distance.xvg
 # histogram of the distance between Lys166 NZ and Asp229 OH with bin width of 0.2 nm, output in xvg format
-apptainer exec $GROMACS_CONTAINER gmx_mpi analyze -f lys166_asp229_distance.xvg -dist lys166_asp229_hist.xvg -bw 0.2
-python $scripts/plotxvg_hist.py lys166_asp229_hist.xvg
+#apptainer exec $GROMACS_CONTAINER gmx_mpi analyze -f lys166_asp229_distance.xvg -dist lys166_asp229_hist.xvg -bw 0.2
+#python $scripts/plotxvg_hist.py lys166_asp229_hist.xvg
 # RSMF of CA atoms of the whole protein, output in xvg format
-echo 3 | apptainer exec $GROMACS_CONTAINER gmx_mpi rmsf -f md_fit.xtc -s $tpr -o rmsf_Ca.xvg -n index.ndx -b 20000 -res  # start at 20 ns (time in ps)
-python $scripts/plot_RMSF_red.py rmsf_Ca.xvg
+#echo 3 | apptainer exec $GROMACS_CONTAINER gmx_mpi rmsf -f md_fit.xtc -s $tpr -o rmsf_Ca.xvg -n index.ndx -b 20000 -res  # start at 20 ns (time in ps)
+#python $scripts/plot_RMSF_red.py rmsf_Ca.xvg
 ### define frames with distance between Lys166 NZ and Asp229 OH < 0.6 nm as "closed" and >= 0.6 nm as "open", output in xvg format
 # Compute distance time series (ps)
-apptainer exec $GROMACS_CONTAINER gmx_mpi distance -f md_fit.xtc -s $tpr -n index.ndx -select 'com of group 24 plus com of group 25' -oall dist_k167_y259_ps.xvg
+#apptainer exec $GROMACS_CONTAINER gmx_mpi distance -f md_fit.xtc -s $tpr -n index.ndx -select 'com of group 24 plus com of group 25' -oall dist_k167_y259_ps.xvg
 # Create new trajectory with selected frames where distance < 0.6 nm
-echo 0 | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f md_fit.xtc -s $tpr -o md_closed.xtc -drop dist_k167_y259_ps.xvg -dropover 0.6
+#echo 0 | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f md_fit.xtc -s $tpr -o md_closed.xtc -drop dist_k167_y259_ps.xvg -dropover 0.6
 # how many frames in closed trajectory vs full?
-apptainer exec $GROMACS_CONTAINER gmx_mpi check -f1 md_fit.xtc -f2 md_closed.xtc
+apptainer exec $GROMACS_CONTAINER gmx_mpi check -f md_fit.xtc
+apptainer exec $GROMACS_CONTAINER gmx_mpi check -f md_closed.xtc
 
 # PCA
 # Compute covariance matrix
