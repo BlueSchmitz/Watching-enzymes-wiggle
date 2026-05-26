6804332d1f94793d1b13183435ffba0fac28b5a8
BlueSchmitz <lismitz99@googlemail.com>
2026-05-26 18:51:37 +0200

    make space

--- MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
+++ MD_simulations/projects/1JCLm/bash_scripts/store_data_TU.sh
@@ -11,28 +11,28 @@
 #SBATCH --mail-user=blueschmitz@tudelft.nl
 #SBATCH --output=copy_to_TU_%j.out
 
-#SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA"
+SRC="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA/outputs/6_HREX/analysis"
 #SRC="/gpfs/work1/0/prjs2080/BtDERA/outputs/6_HREX/rep1.00"
-#DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA"
+DEST="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA/outputs/6_HREX/analysis"
 #DEST="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA/outputs/6_HREX/rep1.00"
 
-#echo "HOSTNAME: $(hostname)"
-#echo "PATH: $PATH"
-#which rclone || echo "RCLONE NOT FOUND"
-#rclone version || echo "RCLONE NOT RUNNING"
+echo "HOSTNAME: $(hostname)"
+echo "PATH: $PATH"
+which rclone || echo "RCLONE NOT FOUND"
+rclone version || echo "RCLONE NOT RUNNING"
 
-#rclone copy "$SRC" "$DEST" \
-#    -P \
-#    -vv \
-#    --transfers=8 \
-#    --checkers=16 \
-#    --timeout=1h \
-#    --retries=10 \
-#    --retries-sleep=30s \
-#    --log-file=rclone_${SLURM_JOB_ID}.log \
+rclone copy "$SRC" "$DEST" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
 
-SRC2="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BtDERA"
-DEST2="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA"
+SRC2="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/BbDERA/outputs/6_HREX/analysis"
+DEST2="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BbDERA/outputs/6_HREX/analysis"
 
 echo "HOSTNAME: $(hostname)"
 echo "PATH: $PATH"
@@ -50,15 +50,56 @@ rclone copy "$SRC2" "$DEST2" \
     --log-file=rclone_${SLURM_JOB_ID}.log \
 
 
-#SRC3="/gpfs/work1/0/prjs2080/BtDERA/outputs/6_HREX"
-#DEST3="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/BtDERA/outputs/6_HREX"
+SRC3="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/CbDERA/outputs/6_HREX/analysis"
+DEST3="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/CbDERA/outputs/6_HREX/analysis"
 
-#rclone copy "$SRC3" "$DEST3" \
-#    -P \
-#    -vv \
-#    --transfers=8 \
-#    --checkers=16 \
-#    --timeout=1h \
-#    --retries=10 \
-#    --retries-sleep=30s \
-#    --log-file=rclone_${SLURM_JOB_ID}.log \
+echo "HOSTNAME: $(hostname)"
+echo "PATH: $PATH"
+which rclone || echo "RCLONE NOT FOUND"
+rclone version || echo "RCLONE NOT RUNNING"
+
+rclone copy "$SRC3" "$DEST3" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
+
+SRC4="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/MlDERA/outputs/6_HREX/analysis"
+DEST4="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/MlDERA/outputs/6_HREX/analysis"
+
+echo "HOSTNAME: $(hostname)"
+echo "PATH: $PATH"
+which rclone || echo "RCLONE NOT FOUND"
+rclone version || echo "RCLONE NOT RUNNING"
+
+rclone copy "$SRC4" "$DEST4" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
+
+SRC5="/home/rleveson/Blue/Watching-enzymes-wiggle/MD_simulations/projects/1JCLm/outputs/6_HREX/analysis"
+DEST5="tudelft_sftp:/staff-umbrella/biocat dera/MD_simulations/1JCLm/outputs/6_HREX/analysis"
+
+echo "HOSTNAME: $(hostname)"
+echo "PATH: $PATH"
+which rclone || echo "RCLONE NOT FOUND"
+rclone version || echo "RCLONE NOT RUNNING"
+
+rclone copy "$SRC5" "$DEST5" \
+    -P \
+    -vv \
+    --transfers=8 \
+    --checkers=16 \
+    --timeout=1h \
+    --retries=10 \
+    --retries-sleep=30s \
+    --log-file=rclone_${SLURM_JOB_ID}.log \
