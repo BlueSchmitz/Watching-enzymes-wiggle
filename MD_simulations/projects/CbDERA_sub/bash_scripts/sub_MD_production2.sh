cfbfca2b4c35bef14b6ae7ca5e7e8f04b8f121fc
BlueSchmitz <lismitz99@googlemail.com>
2026-05-27 15:19:51 +0200

    set up MD CbDERA_sub

---
+++ MD_simulations/projects/CbDERA_sub/bash_scripts/sub_MD_production2.sh
@@ -0,0 +1,121 @@
+#!/bin/bash  
+
+#SBATCH -J CbDERA_sub2_simple_MD
+#SBATCH -t 72:00:00
+#SBATCH -p rome
+#SBATCH -N 1
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task 1
+#SBATCH --gpus=0
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=blueschmitz@tudelft.nl
+#SBATCH --output=sub_MD2_%j.out
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_TASKS=$SLURM_NTASKS
+
+: '
+Folder structure:
+.
+└── MD_simulations/
+    ├── scripts /
+    │   ├── model_missing_residues.py
+    │   ├── get_ali_file.py
+    │   ├── compare_protonation.py
+    │   ├── plot_xvg.py
+    │   ├── scale_all_residues.py
+    │   ├── scale_residues.py
+    │   └── energy_comparison.py
+    ├── mdp_templates/
+    │   ├── minim.mdp
+    │   ├── nvt.mdp
+    │   ├── npt.mdp
+    │   ├── sanity_check.mdp
+    │   └── hrex.mdp
+    ├── force_fields/
+    │   ├── amber99sb-star-ildnp.ff/
+    └── projects/
+        └── 5EKY_monomeric/
+            ├── bash_scripts
+            ├── inputs/
+            │   └── pdb
+            └── outputs/
+                ├── 1_protonation
+                ├── 2_parametrization
+                ├── 3_minimization
+                ├── 4_equilibration
+                ├── 5_sanity_checks/
+                │   ├── rep0
+                │   └── rep1
+                └── 6_HREX/
+                    ├── rep0.60
+                    ├── rep0.63
+                    └── ...
+
+Run this script from the projects/5EKY_monomeric/ directory, it contains relative paths
+'
+# Exit immediately on errors, undefined vars, or failed pipes
+set -euo pipefail
+set -o errtrace
+
+### Project-specific settings ###
+project_dir=CbDERA_sub
+output_dir=7_simple_MD/rep2
+pH=7
+
+export GMXLIB=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/force_fields
+export GROMACS_CONTAINER=$HOME/Blue/software/apptainer_2021/gromacs_plumed.sif
+export PDB2PQR_CONTAINER=$HOME/Blue/software/apptainer_pdb2pqr/pdb2pqr.sif
+export PLUMED_CONTAINER=$HOME/Blue/software/apptainer_plumed/plumed.sif
+
+mdp=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/mdp_templates
+scripts=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/scripts
+pdb=$HOME/Blue/Watching-enzymes-wiggle/MD_simulations/projects/$project_dir/inputs/*.pdb
+
+# Create temporary directory on scratch for this job
+tmpdir=$(mktemp -d /gpfs/scratch1/shared/rleveson/Blue/tmp.XXXXXX)
+mkdir -p "$tmpdir/MD_simulations/projects/"
+
+# Load modules:  
+module load 2023
+module load Python/3.11.3-GCCcore-12.3.0
+module load matplotlib/3.7.2-gfbf-2023a
+module load scikit-learn/1.3.1-gfbf-2023a
+module list
+
+### Copy project to scratch ###
+echo "=== Copying project to scratch ==="
+cp -r /gpfs/work1/0/prjs2080/$project_dir "$tmpdir/MD_simulations/projects/"
+cd $tmpdir/MD_simulations/projects/$project_dir
+
+# Function to copy back results when error occurs and before the script exits
+function copy_back_results {
+    set +e +u # Disable exit on error for this function
+    echo "=== Copying results back to project folder at $(date). ==="
+    if [[ -d "$tmpdir/MD_simulations/projects/$project_dir/outputs/$output_dir" ]]; then
+        rsync -av --partial --inplace \
+          "$tmpdir/MD_simulations/projects/$project_dir/outputs/$output_dir/" \
+          "/gpfs/work1/0/prjs2080/$project_dir/outputs/$output_dir/"
+        echo "=== Copy complete at $(date) ==="
+    else
+        echo "Nothing to copy back (outputs directory not found)"
+    fi
+    # Clean up temporary directory
+    rm -rf "$tmpdir"
+    echo "=== Temporary directory cleaned up at $(date). ==="
+}
+trap copy_back_results EXIT
+
+mkdir -p ./outputs/$output_dir
+
+### Simple MD production run ###
+echo "============= Simple MD production run with GROMACS ============="
+cp ./outputs/4_equilibration/npt_5.gro ./outputs/$output_dir/npt_pro.gro
+cp ./outputs/4_equilibration/topol_5.top ./outputs/$output_dir/topol_pro.top
+cd ./outputs/$output_dir/
+# remove any line that includes the posre file from the topology file
+sed -i '/posre_5.itp/d' topol_pro.top
+# run simple MD production
+apptainer exec $GROMACS_CONTAINER gmx_mpi grompp -f $mdp/classical_MD.mdp -c npt_pro.gro -p topol_pro.top -o md2.tpr 
+apptainer exec $GROMACS_CONTAINER mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm md2 -cpt 15 -maxh 71
+echo "Simple MD production run 2 finished."
