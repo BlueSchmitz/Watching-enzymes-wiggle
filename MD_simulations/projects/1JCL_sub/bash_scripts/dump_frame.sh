e3a96ea7e4fd8cb5d307af45dd5c3ed087c576be
BlueSchmitz <lismitz99@googlemail.com>
2026-06-10 00:22:17 +0200

    extract gro

--- MD_simulations/projects/1JCL_sub/bash_scripts/dump_frame.sh
+++ MD_simulations/projects/1JCL_sub/bash_scripts/dump_frame.sh
@@ -78,6 +78,4 @@ echo "============= Downsizing and Exporting trajectory ============="
 #echo "Trajectory saved as md_1000.xtc"
 
 # extract closed conformations
-echo -e "0\n0" | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f md_closed_rep3.xtc -s ./rep3/md3.tpr -o frame_closed.pdb -n index.ndx -dump 35000
-
-apptainer exec $GROMACS_CONTAINER gmx_mpi grompp -f $mdp/classical_MD.mdp -c frame_closed.pdb -p ./rep3/topol_pro.top -n index.ndx -o closed.tpr -maxwarn 1
+echo -e "0\n0" | apptainer exec $GROMACS_CONTAINER gmx_mpi trjconv -f md_closed_rep3.xtc -s ./rep3/md3.tpr -o closed_start.gro -n index.ndx -dump 35000
